1f40fb753f8d6d8d2158619c4220efda0f26dc11
bigyihsuan <bigyihsuan@gmail.com>
2020-07-18 20:14:52 -0400

    initial code

---
+++ SuperRemote/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace SuperRemote
+{
+	static class Main
+	{
+		static void Load(UnityModManager.ModEntry modEntry)
+		{
+			Debug.Log("SuperRemote loaded");
+			var harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+			Debug.Log("SuperRemote patched things!");
+		}
+	}
+	// Forever set the signal to its max
+	[HarmonyPatch(typeof(LocomotiveRemoteController), "UpdateSignal")]
+	class LocomotiveRemoteController_UpdateSignal_Patch
+	{
+		static void Prefix(ref float ___signal)
+		{
+			___signal = 1f;
+		}
+	}
+	// Forever set the battery to its max capacity
+	[HarmonyPatch(typeof(LocomotiveRemoteController), "UpdateSolarPanel")]
+	class LocomotiveRemoteController_UpdateSolarPanel_Patch
+	{
+		static void Prefix(ref LocomotiveRemoteController __instance)
+		{
+			__instance.battery = 1f;
+		}
+	}
+
+
+}
